c5e7d273549366dacd7dc170b120afae2e494c74
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-05-25 00:31:33 +0530

    Extract calculation into a separate function
    The part of main which prompted the user for input, accepted input,
    calculated the result, and displayed it, has been moved into a separate
    function called `calculate.`

--- main/pcalc.cpp
+++ main/pcalc.cpp
@@ -4,38 +4,45 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "parser/parser.hpp"
 
 using std::cout;
 using std::cin;
 using std::string;
+using std::exit;
 
-int main()
+void calculate(Parser& calc)
 {
     constexpr auto prompt = "> ";
     constexpr auto answer = "= ";
     constexpr auto error = "! ";
 
+    cout << prompt;
+    string expr;
+    cin >> expr;
+
+    if (cin.eof())
+    {
+        cout << "\nBye!\n";
+        exit(EXIT_SUCCESS);
+    }
+    if (cin.bad())
+    {
+        cout << error << "Can't read input! Terminating...\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    cout << answer << calc.evaluate(expr) << "\n";
+}
+
+int main()
+{
     cout << "Welcome to Power Calculator!\n";
     Parser calc;
 
     while (true)
     {
-        cout << prompt;
-        string expr;
-        cin >> expr;
-
-        if (cin.eof())
-        {
-            cout << "\nBye!\n";
-            break;
-        }
-        if (cin.bad())
-        {
-            cout << error << "Can't read input! Terminating...\n";
-            break;
-        }
-        
-        cout << answer << calc.evaluate(expr) << "\n";
+        calculate(calc);
     }
 }
