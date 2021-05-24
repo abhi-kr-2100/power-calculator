872e41b7affa85d2012296ddf4292e40e115cb96
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-05-24 21:46:42 +0530

    Make pcalc output what the user typed in
    This version of pcalc simply outputs back what the user typed in as
    input. It displays the correct prompts and error messages on
    input failure or EOF.

--- main/pcalc.cpp
+++ main/pcalc.cpp
@@ -3,10 +3,37 @@
 */
 
 #include <iostream>
+#include <string>
 
 using std::cout;
+using std::cin;
+using std::string;
 
 int main()
 {
-    cout << "Hello, world!\n";
+    constexpr auto prompt = "> ";
+    constexpr auto answer = "= ";
+    constexpr auto error = "! ";
+
+    cout << "Welcome to Power Calculator!\n";
+
+    while (true)
+    {
+        cout << prompt;
+        string expr;
+        cin >> expr;
+
+        if (cin.eof())
+        {
+            cout << "\nBye!\n";
+            break;
+        }
+        if (cin.bad())
+        {
+            cout << error << "Can't read input! Terminating...\n";
+            break;
+        }
+        
+        cout << answer << expr << "\n";
+    }
 }
