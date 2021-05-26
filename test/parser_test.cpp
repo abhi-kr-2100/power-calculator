677afbb46251f038907346b09a46c25c4746fee7
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-05-26 19:06:07 +0530

    Add a test for inputs like: "(3)(2)"

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -94,6 +94,7 @@ TEST(ParserPrimaryTest, BadParens)
     EXPECT_THROW(calc.evaluate("(5)+5)"), Unmatched_parentheses);
     EXPECT_THROW(calc.evaluate("()"), Syntax_error);
     EXPECT_THROW(calc.evaluate("5("), Syntax_error);
+    EXPECT_THROW(calc.evaluate("(5)(5)"), Syntax_error);
 }
 
 TEST(ParserTermTest, CorrectTerms)
