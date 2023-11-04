f27aa79698e5ac79c7783522dc950271ec15754d
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-11-04 20:44:51 +0530

    Add exceptions thrown by unit conversion
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- primary/exceptions.hpp
+++ primary/exceptions.hpp
@@ -21,4 +21,38 @@ private:
     std::string what_err;
 };
 
+class Incompatible_units : public std::exception
+{
+public:
+    Incompatible_units(const std::string &s = "")
+        : what_err{s}
+    {
+    }
+
+    const char *what() const noexcept
+    {
+        return what_err.c_str();
+    }
+
+private:
+    std::string what_err;
+};
+
+class Unknown_unit : public std::exception
+{
+public:
+    Unknown_unit(const std::string &s = "")
+        : what_err{s}
+    {
+    }
+
+    const char *what() const noexcept
+    {
+        return what_err.c_str();
+    }
+
+private:
+    std::string what_err;
+};
+
 #endif
